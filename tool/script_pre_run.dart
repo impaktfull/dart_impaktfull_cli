e7d09c1aecf545a1b7997a7e5ca6d64beb618fcf
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2023-11-27 21:32:17 +0100

    fix: prerun script

--- tool/script_pre_run.dart
+++ tool/script_pre_run.dart
@@ -5,6 +5,6 @@ import 'package:path/path.dart';
 void main(List<String> args) {
   final fileName = 'example_config';
   final basePath = join('example', 'bin');
-  final file = File(join(fileName, basePath));
+  final file = File(join(basePath, fileName));
   file.copySync(join(basePath, '$fileName.dart'));
 }
