c633114ba32c43c06df343dde1f4a4d53651f153
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2025-05-28 14:55:53 +0200

    fix: #3 implemented better fvm project detection (#4)
    * fix: #3 implemented better fvm project detection
    
    * fix: changelog

---
+++ lib/src/core/util/cli_tools/cli_tools_util.dart
@@ -0,0 +1,86 @@
+import 'package:impaktfull_cli/impaktfull_cli.dart';
+import 'package:impaktfull_cli/src/core/model/data/environment/installed_cli_tool.dart';
+import 'package:impaktfull_cli/src/core/model/data/environment/operating_system.dart';
+
+class CliToolsUtil {
+  const CliToolsUtil._();
+
+  static List<InstalledCliTool> getInstalledCliTools(
+          List<InstalledCliTool> allCliTools) =>
+      allCliTools.where((element) => element.isInstalled).toList();
+
+  static List<InstalledCliTool> getNotInstalledCliTools(
+          List<InstalledCliTool> allCliTools) =>
+      allCliTools.where((element) => !element.isInstalled).toList();
+
+  static bool isInstalled(
+    CliTool cliTool,
+    List<InstalledCliTool> allCliTools,
+  ) =>
+      allCliTools.any((element) => element.cliTool == cliTool);
+
+  static Future<List<InstalledCliTool>> checkInstalledTools(
+          ProcessRunner processRunner) async =>
+      CliTool.values
+          .where((element) => element.supportedOperatingSystems
+              .contains(OperatingSystem.current))
+          .map((cliTool) => _isToolInstalled(processRunner, cliTool))
+          .wait;
+
+  static Future<InstalledCliTool> _isToolInstalled(
+      ProcessRunner processRunner, CliTool cliTool) async {
+    try {
+      final result =
+          await processRunner.runProcess(['which', cliTool.commandName]);
+      if (result.isEmpty) {
+        return InstalledCliTool.notInstalled(
+          cliTool: cliTool,
+        );
+      }
+      return InstalledCliTool.installed(
+        cliTool: cliTool,
+        path: result,
+      );
+    } catch (e) {
+      ImpaktfullCliLogger.log(
+          'Failed to check if ${cliTool.commandName} is installed');
+      return InstalledCliTool.notInstalled(
+        cliTool: cliTool,
+      );
+    }
+  }
+
+  static String getCliToolsLog(List<InstalledCliTool> allCliTools) {
+    final sb = StringBuffer();
+    final installedCliTools = getInstalledCliTools(allCliTools);
+    final notInstalledCliTools = getNotInstalledCliTools(allCliTools);
+    sb.writeln('Installed Tools:');
+    for (final cliTool in installedCliTools) {
+      sb.writeln('\t${cliTool.cliTool.commandName} - ${cliTool.path}');
+    }
+    sb.writeln('Not Installed Tools:');
+    for (final cliTool in installedCliTools) {
+      sb.writeln('\t${cliTool.cliTool.commandName} - ${cliTool.path}');
+    }
+    if (installedCliTools.isNotEmpty) {
+      sb.writeln('Installed Tools:');
+      for (final clitool in installedCliTools) {
+        sb.writeln('\t${clitool.cliTool.commandName} - ${clitool.path}');
+      }
+    }
+    if (notInstalledCliTools.isNotEmpty) {
+      sb.writeln('Not Installed Tools:');
+      for (final notInstalledCliTool in notInstalledCliTools) {
+        final cliTool = notInstalledCliTool.cliTool;
+        final cliToolSb = StringBuffer('\t${cliTool.commandName}');
+        final installationInstructions =
+            cliTool.installationInstructions[OperatingSystem.current];
+        if (installationInstructions != null) {
+          cliToolSb.write(' - Install instructions: $installationInstructions');
+        }
+        sb.writeln(cliToolSb.toString());
+      }
+    }
+    return sb.toString();
+  }
+}
---
+++ lib/src/core/util/fvm/fvm_util.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+
+class FvmUtil {
+  const FvmUtil._();
+  static Future<bool> isFvmProject(Directory workingDir) async {
+    final isGitRoot = _isGitRootDirectory(workingDir);
+    if (isGitRoot) {
+      return false;
+    }
+
+    final fvmrcFile = File(join(workingDir.path, '.fvmrc'));
+    if (fvmrcFile.existsSync()) {
+      return true;
+    }
+    // fvm_config.json is the old config file
+    final fvmConfigFile = File(join(workingDir.path, 'fvm', 'fvm_config.json'));
+    if (fvmConfigFile.existsSync()) {
+      return true;
+    }
+    return isFvmProject(workingDir.parent);
+  }
+
+  static bool _isGitRootDirectory(Directory workingDir) {
+    final gitDir = Directory(join(workingDir.path, '.git'));
+    return gitDir.existsSync();
+  }
+}
