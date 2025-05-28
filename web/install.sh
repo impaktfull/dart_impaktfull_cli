a1ca79f0fb534ea8b11e89d7224c286aa258b2a2
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2025-05-29 00:51:00 +0200

    fix: install script to only apply the zshrc patch once

--- web/install.sh
+++ web/install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+if [ ! -f ~/.zshrc ]; then
+    echo "~/.zshrc does not exist, please install zsh and run this script again"
+    exit -1
+fi
+
+INSTALL_DIR="$HOME/.impaktfull/impaktfull_cli"
+
 echo "Downloading impaktfull_cli"
-mkdir -p ~/.impaktfull/impaktfull_cli
-curl -fsSL https://cli.impaktfull.com/download/impaktfull_cli -o ~/.impaktfull/impaktfull_cli/impaktfull_cli
+mkdir -p $INSTALL_DIR
+curl -fsSL https://cli.impaktfull.com/download/impaktfull_cli -o $INSTALL_DIR/impaktfull_cli
 
 echo "Making impaktfull_cli executable"
-chmod +x ~/.impaktfull/impaktfull_cli/impaktfull_cli
+chmod +x $INSTALL_DIR
 
 echo "Adding impaktfull_cli to PATH"
-echo 'export PATH="$HOME/.impaktfull/impaktfull_cli:$PATH"' >> ~/.zshrc
+EXPORT_VALUE="export PATH=\"$INSTALL_DIR:\$PATH\""
+if ! grep -q "$EXPORT_VALUE" ~/.zshrc; then
+    echo "$EXPORT_VALUE" >> ~/.zshrc
+fi
 
 echo "Done"
