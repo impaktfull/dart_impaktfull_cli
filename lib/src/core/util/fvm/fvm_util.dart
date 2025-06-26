5c1de84ed7fd3606d53dbcee55a98dac9944105d
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2025-06-26 20:35:52 +0200

    fix: fvm detection in monorepo (#28)
    * fix: fvm detection in monorepo
    
    * fix: changelog
    
    * fix: comments

--- lib/src/core/util/fvm/fvm_util.dart
+++ lib/src/core/util/fvm/fvm_util.dart
@@ -5,11 +5,6 @@ import 'package:path/path.dart';
 class FvmUtil {
   const FvmUtil._();
   static Future<bool> isFvmProject(Directory workingDir) async {
-    final isGitRoot = _isGitRootDirectory(workingDir);
-    if (!isGitRoot) {
-      return false;
-    }
-
     final fvmrcFile = File(join(workingDir.path, '.fvmrc'));
     if (fvmrcFile.existsSync()) {
       return true;
@@ -19,11 +14,9 @@ class FvmUtil {
     if (fvmConfigFile.existsSync()) {
       return true;
     }
+    if (workingDir.parent.path == workingDir.path) {
+      return false;
+    }
     return isFvmProject(workingDir.parent);
-  }
-
-  static bool _isGitRootDirectory(Directory workingDir) {
-    final gitDir = Directory(join(workingDir.path, '.git'));
-    return gitDir.existsSync();
   }
 }
